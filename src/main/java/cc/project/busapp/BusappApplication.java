94bc9f627a612c14bd49bec91db584656297ae83
danielbc09 <jdanielbc193@gmail.com>
2018-11-15 10:01:05 +0100

    closes #2

---
+++ src/main/java/cc/project/busapp/BusappApplication.java
@@ -0,0 +1,12 @@
+package cc.project.busapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusappApplication.class, args);
+    }
+}
