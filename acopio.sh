25b868c8f1c52f01fbec02d753daa1decc64c17f
danielbc09 <jdanielbc193@gmail.com>
2019-01-22 16:37:38 +0100

    Modificación acopio.sh agregar BD postgres closes #34

--- acopio.sh
+++ acopio.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+#Variables de entorno
+database_user=$AZ_DATABASE_USERADMIN
+database_pass=$AZ_DATABASE_PASSWORD
+
 #logear en consola azure
 az login
 
@@ -5,7 +10,23 @@ az login
 az group create --name ccdaniel --location francecentral
 
 #crear la imagen en ubuntu
-az vm create --resource-group ccdaniel --name usermicroser --image Canonical:UbuntuServer:18.04-LTS:latest --generate-ssh-keys --size Basic_A0
+
+echo "Creando maquina virtual"
+ip=$(az vm create --resource-group ccdaniel --name usermicroservice --image Canonical:UbuntuServer:18.04-LTS:latest --generate-ssh-keys --size Basic_A0 --public-ip-address-allocation static | jq -r '.publicIpAddress')
+
+#imprimir la ip
+echo "La Ip es: "
+echo $ip
 
 #abrir el puerto 80
-az vm open-port --resource-group ccdaniel --name usermicroser --port 80
+echo "Abriendo puerto 80"
+az vm open-port --resource-group ccdaniel --name usermicroservice --port 80
+
+#Creando base de datos PostgresSQL
+echo "Creando Base de datos"
+az postgres server create --resource-group ccdaniel --name postgrescc --location francecentral --admin-user $database_user --admin-password $database_pass --sku-name B_Gen5_1 --version 9.6
+
+
+#Configurando reglas del firewall
+echo "Añadiendo regla de firewall para permitir conexiones externas de solo la ip especificada"
+az postgres server firewall-rule create --resource-group ccdaniel --server postgrescc --name AllowMyIP --start-ip-address $ip --end-ip-address $ip
